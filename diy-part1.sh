6cee468b6b1e384ce62070c54cc0df76d1010b38
CCA2878 <51525400+CCA2878@users.noreply.github.com>
2022-03-06 22:52:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,16 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+rm -rf feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git thirdpartypkgs https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+echo 'src-git ledepkges https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git ledeluci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git ledepkges https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+
+
 
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
